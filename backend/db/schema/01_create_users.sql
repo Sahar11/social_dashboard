634f9de9d7c80aa810a03911713cb6a0096a425d
Sahar11 <saharhalim151@gmail.com>
2022-05-04 14:26:51 -0400

    add schema

---
+++ backend/db/schema/01_create_users.sql
@@ -0,0 +1,12 @@
+-- schema/01_create_users.sql
+DROP TABLE IF EXISTS users CASCADE;
+-- CREATE USERS
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  firstname VARCHAR(255) NOT NULL,
+  lastname VARCHAR(255) NOT NULL,
+  gender VARCHAR(15) NOT NULL,  
+  dob VARCHAR(20) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  pwd VARCHAR(255)
+);
---
+++ backend/db/schema/02_create_messages.sql
@@ -0,0 +1,9 @@
+-- schema/02_create_messages.sql
+DROP TABLE IF EXISTS messages CASCADE;
+-- CREATE USERS
+CREATE TABLE messages (
+  id SERIAL PRIMARY KEY,
+  message VARCHAR(400) NOT NULL,
+  user_id integer REFERENCES users(id) ON DELETE CASCADE NOT NULL,
+  date DATE
+);
