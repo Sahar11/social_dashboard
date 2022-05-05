436f4fb8ac8b0cebcb02a8ca62ae33d6f127dbba
Sahar11 <saharhalim151@gmail.com>
2022-05-05 14:07:09 -0400

    add useEffect in App.js

--- social_dashboard/src/App.js
+++ social_dashboard/src/App.js
@@ -1,11 +1,24 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
+
 function App() {
-  return (
-    <div className="App">
-     
-    </div>
+  const [state, setState] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/users")
+      .then((res) => {
+        console.log("response", res.data);
+        setState(res.data);
+      })
+      .catch((error) => console.log(`Error: ${error}`));
+  }, []);
+  
+  return ( state.map((user) => {
+    <div key={user.id}>user.firstname</div>
+  })
+    
   );
 }
 
