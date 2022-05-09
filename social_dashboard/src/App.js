ca3e340f83e25ff700d181603d2699420bb6ea54
Sahar11 <saharhalim151@gmail.com>
2022-05-09 10:41:33 -0400

    add material ui

--- social_dashboard/src/App.js
+++ social_dashboard/src/App.js
@@ -1,24 +1,14 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import './App.css';
+import Home from './components/Home'
 
 
 function App() {
-  const [state, setState] = useState([]);
-  useEffect(() => {
-    axios
-      .get("http://localhost:3000/users")
-      .then((res) => {
-        console.log("response", res.data);
-        setState(res.data);
-      })
-      .catch((error) => console.log(`Error: ${error}`));
-  }, []);
+ 
   
-  return ( state.map((user) => {
-    <div key={user.id}>user.firstname</div>
-  })
-    
+   return ( 
+   <Home/>
   );
 }
 
