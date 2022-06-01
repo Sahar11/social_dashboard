df265f6d0d5547464b09ddd3baa738fba3c83605
Sahar11 <saharhalim151@gmail.com>
2022-06-01 17:42:36 -0400

    work on navbar

--- social_dashboard/src/App.js
+++ social_dashboard/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import New from './components/New';
 import Body from './components/Table';
 import './App.css';
 const linkArray = ["Products", "Services", "Overview", "Contact Us"];
@@ -9,8 +10,8 @@ const App = () => {
 
     return (
         <>
-         <Body /> 
-       <Navbar links={linkArray}/>
+         <New /> 
+       {/* <Navbar links={linkArray}/> */}
      
       
        </>
--- social_dashboard/src/components/table.js
+++ social_dashboard/src/components/table.js
@@ -1,7 +1,15 @@
 import { Grid } from "@mui/material";
 import React from "react";
-import Item from '@mui/material/ListItem';
+import { styled } from '@mui/material/styles';
+import { Paper } from "@mui/material";
 import { Box } from "@mui/material";
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 export default function Table() {
 
