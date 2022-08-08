263f2bdc6b1c6c7f18b91911dcf278f26b00761e
Sahar11 <saharhalim151@gmail.com>
2022-08-08 15:58:46 -0400

    get book data from google bookm api

--- social_dashboard/src/pages/Books/Books.jsx
+++ social_dashboard/src/pages/Books/Books.jsx
@@ -3,19 +3,21 @@ import axios from "axios";
 import Search from "./SearchBar";
 
 export default function Books(props) {
-  const [search, setSearch] = useState({});
+  const [search, setSearch] = useState();
+  const [books, setBooks] = useState([]);
 
   const searchBooks =(e) => {
     let url = "https://www.googleapis.com/books/v1/volumes?q="+ search;
     e.preventDefault();
   axios 
   .get(url)
-   .then((data) => 
-   console.log(data))
+  .then((res) => {
+    console.log(...res.data.items);
+   setBooks(...res.data.items);
+  })
   }
-
   function handleSearch(e) {
-    console.log(e.target.value);
+    //console.log(e.target.value);
     setSearch(e.target.value);
   }
   return (
